85664f4dac3ef97e008c10a130a3fc784298533d
KrisnaDiva <krisnadiva0412@gmail.com>
2024-04-19 00:56:34 +0700

    create : Email and Password CustomView

---
+++ app/src/main/java/com/krisna/diva/storyapp/ui/customview/EmailEditText.kt
@@ -0,0 +1,20 @@
+package com.krisna.diva.storyapp.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Patterns
+import com.google.android.material.textfield.TextInputEditText
+import com.krisna.diva.storyapp.R
+
+class EmailEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : TextInputEditText(context, attrs) {
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        if (s.toString().isNotEmpty() && !Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+            setError(context.getString(R.string.email_error), null)
+        } else {
+            error = null
+        }
+    }
+}
