4147e946ddf3bc588f733135918608778d64f00e
KrisnaDiva <krisnadiva0412@gmail.com>
2024-04-18 14:10:15 +0700

    create : Base Project

---
+++ app/src/main/java/com/krisna/diva/storyapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.krisna.diva.storyapp
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.krisna.diva.storyapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.background = null
+        navView.setupWithNavController(navController)
+
+        binding.fabAddStory.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Tombol Di Klik", Toast.LENGTH_LONG).show()
+        }
+    }
+}
